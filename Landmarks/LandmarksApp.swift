709436cda6b9cf138c6825f9a904ac05619c0ea5
Benjamin <benjamingirard25@gmail.com>
2025-05-16 12:07:39 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Benjamin Girard on 16/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
